3cde890acc3c49400efdfbf13512abeb61252a2d
Estevan Bellino <bellino.estevan@gmail.com>
2018-04-21 00:56:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,50 +1,11 @@
-var playlist = {
-  artist1: "song1",
-  artist2: "song2",
-  artist3: "song3"
-};
+var playlist = {artist: 'songTitle'}
 
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
 }
 
-updatePlaylist(playlist, "artist4", "song4");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
 }
-
-removeFromPlaylist(playlist, "artist2");
-
-  it('defines an object called `playlist` containing at least one artist-song pair', () => {
-    expect(typeof playlist).to.equal('object')
-    expect(Object.keys(playlist).length).to.be.greaterThan(0)
-  })
-
-  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    before(() => {
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
-
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-
-      expect(playlist).
-        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-    })
-  })
-
-  describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      removeFromPlaylist(playlist, 'Slowdive')
-
-      expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-      expect(playlist).
-        not.to.have.all.keys({'Slowdive': 'Alison'})
-    })
-  })
-})
